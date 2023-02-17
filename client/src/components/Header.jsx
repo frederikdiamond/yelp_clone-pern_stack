219c686ecf767c2bad1cb7d2499d37c73e41b296
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-02-17 20:24:03 +0100

    React components finished.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h1 className='font-weigh-light display-1 text-center'>Restaurant Finder</h1>
+        </div>
+    )
+}
