04fd48736a1c3aa452ab36199aed4f9ec41c87e7
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-02-17 23:32:00 +0100

    Fetching data.

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import RestaurantFinder from '../apis/RestaurantFinder'
+import { RestaurantsContext } from '../context/RestaurantsContext'
 
-export const RestaurantList = () => {
+export const RestaurantList = (props) => {
+    const { restaurants, setRestaurants } = useContext(RestaurantsContext)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await RestaurantFinder.get("/")
+                setRestaurants(response.data.data.restaurants)
+            } catch (err) {
+
+            }
+        }
+
+        fetchData()
+    }, [])
+
     return (
         <div className='list-group'>
             <table className="table table-hover table-dark">
