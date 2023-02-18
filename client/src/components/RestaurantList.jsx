ece7d1344164a2dfddceafc7f751b93eb60bbecf
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-02-18 20:44:24 +0100

    Minor changes

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -17,6 +17,17 @@ const RestaurantList = (props) => {
         fetchData()
     }, [])
 
+    const handleDelete = async (id) => {
+        try {
+            const response = await RestaurantFinder.delete(`/${id}`)
+            setRestaurants(restaurants.filter((restaurant) => {
+                return restaurant.id !== id
+            }))
+        } catch (err) {
+
+        }
+    }
+
     return (
         <div className='list-group'>
             <table className="table table-hover table-dark">
@@ -42,7 +53,7 @@ const RestaurantList = (props) => {
                                     <button className="btn btn-warning">Update</button>
                                 </td>
                                 <td>
-                                    <button className="btn btn-danger">Delete</button>
+                                    <button onClick={(e) => handleDelete(e, restaurant.id)} className="btn btn-danger">Delete</button>
                                 </td>
                             </tr>
                         )
