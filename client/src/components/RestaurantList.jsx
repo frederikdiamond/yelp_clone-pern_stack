91caac00e5f813007f2990c4e0cbcca0856e21f8
FrederikDiamonD <frederikhandberg02@gmail.com>
2023-02-19 17:45:44 +0100

    Working on Add Review.

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -38,6 +38,10 @@ const RestaurantList = (props) => {
         }
     }
 
+    const handleRestaurantSelect = (id) => {
+        navigate(`/restaurants/${id}`)
+    }
+
     return (
         <div className='list-group'>
             <table className="table table-hover table-dark">
@@ -54,7 +58,7 @@ const RestaurantList = (props) => {
                 <tbody>
                     {restaurants && restaurants.map((restaurant) => {
                         return (
-                            <tr key={restaurant.id}>
+                            <tr onClick={() => handleRestaurantSelect(restaurant.id)} key={restaurant.id}>
                                 <td>{restaurant.name}</td>
                                 <td>{restaurant.location}</td>
                                 <td>{"$".repeat(restaurant.price_range)}</td>
--- client/src/context/RestaurantsContext.jsx
+++ client/src/context/RestaurantsContext.jsx
@@ -4,12 +4,14 @@ export const RestaurantsContext = createContext()
 
 export const RestaurantsContextProvider = (props) => {
     const [restaurants, setRestaurants] = useState([])
+    const [selectedRestaurant, setSelectedRastaurant] = useState(null)
+
     const addRestaurants = (restaurant) => {
         setRestaurants([...restaurants, restaurant])
     }
 
     return (
-        <RestaurantsContext.Provider value={{ restaurants, setRestaurants, addRestaurants }}>
+        <RestaurantsContext.Provider value={{ restaurants, setRestaurants, addRestaurants, selectedRestaurant, setSelectedRastaurant }}>
             {props.children}
         </RestaurantsContext.Provider>
     )
